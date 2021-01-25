82123aeee89c245ea2e7381b015cd4de361403bd
Joshua Rose <Joshua.rose1@outlook.com>
2021-01-25 16:15:56 -0600

    📦adding additoinal packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
