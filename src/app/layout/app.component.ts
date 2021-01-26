123ceb784107160c72c925621b51a510ac8ba4f7
Joshua Rose <Joshua.rose1@outlook.com>
2021-01-26 14:07:32 -0600

    reformat to follow the guide

--- src/app/layout/app.component.ts
+++ src/app/layout/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import {startWith, delay} from 'rxjs/operators'
+import { Helpers } from "../helpers/helpers";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'ToptalNgDotNet-bitly';
+export class AppComponent implements AfterViewInit {
+  subscription: Subscription;
+  authentication: boolean;
+  constructor(private helpers: Helpers){
+
+  }
+  ngAfterViewInit() {
+    this.subscription = this.helpers.isAuthenticationChanged().pipe(
+      startWith( this.helpers.isAuthenticated()),
+      delay(0)).subscribe((value) => this.authentication = value)
+    
+  }
+  title= 'Angular x Seed';
+  ngOnDistroy() {
+    this.subscription.unsubscribe()
+  }
 }
