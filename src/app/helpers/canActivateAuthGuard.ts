7913c1e860dc29e4f134ed0117f8c1ae6c681fb2
Joshua Rose <Joshua.rose1@outlook.com>
2021-01-26 10:30:32 -0600

    ✨create canActivateAuthGuard.ts

---
+++ src/app/helpers/canActivateAuthGuard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Helpers } from './helpers';
+import { Observable } from 'rxjs';
+@Injectable();
+
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router, private helper: Helpers) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+        if (!this.helper.isAuthenticated()) {
+            this.router.navigate(['/login']);
+            return false;
+        }
+        return true;
+    }
+}
+
